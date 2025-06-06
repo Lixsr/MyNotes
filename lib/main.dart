2c08532cd70d75f70d940d4cd40c4b4bf56650a4
Osamah ALNAHARI <osamah@Osamahs-MacBook-Air.local>
2025-06-06 13:06:35 +0300

    firebase currentUser

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes/firebase_options.dart';
 
 // Views
 import 'package:notes/view/login_view.dart';
@@ -19,9 +22,43 @@ void main() {
           foregroundColor: Colors.black,
         ),
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                return const Text("You are logged in.");
+              } else {
+                if (user?.emailVerified ?? false) {
+                  print("Email is verified.");
+                } else {
+                  print("Email is not verified.");
+                }
+              }
+              return const Text("Firebase initialized successfully.");
+            default:
+              return const Text(
+                "Something went wrong, please try again later.",
+              );
+          }
+        },
+      ),
+    );
+  }
+}
